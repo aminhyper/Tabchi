60c10135e33a67e6f619e7de9d0c9b00efe9128a
aminhyper <bartar2011@yahoo.com>
2019-09-02 21:23:40 +0430

    connect start

--- Robot.py
+++ Robot.py
@@ -1,2 +1,8 @@
-import telethon
-
+from telethon import TelegramClient, events,sync
+from Config import *
+from telethon.tl.types import *
+from telethon.tl.functions.contacts import ResolveUsernameRequest
+from telethon.tl.functions.channels import GetAdminLogRequest
+NAME = 'tabchi'
+client = TelegramClient(NAME, TELEGRAM_APP_API_ID, TELEGRAM_APP_API_HASH)
+client.start()
